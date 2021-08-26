76e89ea7446c6f19c8340cfdd5f1e3a2c82e682f
n.abbasi <nimaab1992@gmail.com>
2021-08-26 19:40:53 +0430

    create mock data for carousel and navigation

---
+++ src/data/mockData.js
@@ -0,0 +1,62 @@
+export const CarouselImages =[
+  {
+      url: "/images/slider/1.jpg",
+      title: "test",
+    },
+    {
+      url: "/images/slider/2.jpg",
+      title: "test",
+    },
+    {
+      url: "/images/slider/3.jpg",
+      title: "test",
+    },
+   
+ 
+]
+export const navbarItems = [
+  {
+      id:0,
+      title: 'Alfa Romeo',
+      image: '/images/alfa.jpg'
+  },
+  {
+      id:1,
+      title: 'Alpine',
+      image: '/images/alpine.jpg'
+  },
+  {
+      id:2,
+      title: 'Aston Martin',
+      image: '/images/aston.jpg'
+  },
+  {
+      id:3,
+      title: 'Scuderia AlphaTauri',
+      image: '/images/honda.jpg'
+  }, {
+      id:4,
+      title: 'Scuderia Ferrari',
+      image: '/images/ferari.jpg'
+  }, {
+      id:5,
+      title: 'Haas',
+      image: '/images/hass.jpg'
+  }, {
+      id:6,
+      title: 'McLaren',
+      image: '/images/maclaren.jpg'
+  }, {
+      id:7,
+      title: 'Mercedes-AMG Petronas',
+      image: '/images/mercedess.jpg'
+  }, {
+      id:8,
+      title: 'Red Bull Racing Honda',
+      image: '/images/redbull.jpg'
+  }, {
+      id:9,
+      title: 'Williams',
+      image: '/images/william.jpg'
+  }
+]
--- src/layout/MainNav.js
+++ src/layout/MainNav.js
@@ -3,71 +3,10 @@
 import Navigation from '../components/navigation/Navigation';
 import classes from './MainNav.module.css';
 import Carousel from '../components/slider/Carousel';
-
+import { CarouselImages,navbarItems } from "../data/mockData";
 
 function MainNavigation () {
-    const images =[
-        {
-            url: "/images/slider/1.jpg",
-            title: "test",
-          },
-          {
-            url: "/images/slider/2.jpg",
-            title: "test",
-          },
-          {
-            url: "/images/slider/3.jpg",
-            title: "test",
-          },
-         
-       
-    ]
-    const navbarItems = [
-        {
-            id:0,
-            title: 'Alfa Romeo',
-            image: '/images/alfa.jpg'
-        },
-        {
-            id:1,
-            title: 'Alpine',
-            image: '/images/alpine.jpg'
-        },
-        {
-            id:2,
-            title: 'Aston Martin',
-            image: '/images/aston.jpg'
-        },
-        {
-            id:3,
-            title: 'Scuderia AlphaTauri',
-            image: '/images/honda.jpg'
-        }, {
-            id:4,
-            title: 'Scuderia Ferrari',
-            image: '/images/ferari.jpg'
-        }, {
-            id:5,
-            title: 'Haas',
-            image: '/images/hass.jpg'
-        }, {
-            id:6,
-            title: 'McLaren',
-            image: '/images/maclaren.jpg'
-        }, {
-            id:7,
-            title: 'Mercedes-AMG Petronas',
-            image: '/images/mercedess.jpg'
-        }, {
-            id:8,
-            title: 'Red Bull Racing Honda',
-            image: '/images/redbull.jpg'
-        }, {
-            id:9,
-            title: 'Williams',
-            image: '/images/william.jpg'
-        }
-    ]
+    
     return (
        <div>
             <div className={classes.navigation}>
@@ -76,7 +15,7 @@ function MainNavigation () {
                 </div>
                 <div className={classes.navigation__carosel}>
                     
-                    <Carousel images={images} />
+                    <Carousel images={CarouselImages} />
                 </div>
                
                 
